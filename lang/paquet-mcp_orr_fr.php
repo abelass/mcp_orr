b6369d953b8c4435f3e830b7ed3e72fee6f439eb
abelass <websolutions@mychacra.net>
2013-03-18 10:32:25 +0100

    import initial

---
+++ lang/paquet-mcp_orr_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mcp_orr_description' => 'Intègre la gesztion de ressource dans un pack',
+	'mcp_orr_nom' => 'Mcp Orr',
+	'mcp_orr_slogan' => 'Gestion de ressources',
+);
+
+?>
